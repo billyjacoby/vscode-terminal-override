90c0bcddb93705c4de3f87ff3de15e1573b0f135
Billy Jacoby <billyjacoby@gmail.com>
2024-04-01 14:33:16 -0400

    feat: activate on startup, dispose of built in terminal

--- src/extension.ts
+++ src/extension.ts
@@ -21,23 +21,10 @@ export function activate(context: vscode.ExtensionContext) {
     }
     terminal.sendText(terminalCommand);
     terminal.hide();
+    setTimeout(() => {
+      terminal.dispose();
+    }, 2000);
   });
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'terminal-override.activate',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Terminal override has been activated!'
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
