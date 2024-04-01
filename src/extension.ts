7433613ad7fc957a2871580e5d04633834751239
Billy Jacoby <billyjacoby@gmail.com>
2024-04-01 14:12:35 -0400

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Terminal override active! Use the built in terminal binding to open the terminal defined in settings.'
+  );
+
+  vscode.window.onDidOpenTerminal((terminal: vscode.Terminal) => {
+    const terminalCommand: string | undefined = vscode.workspace
+      .getConfiguration('terminal-override')
+      .get('command');
+    if (!terminalCommand) {
+      vscode.window.showErrorMessage('Terminal command not found!');
+      return;
+    }
+    terminal.sendText(terminalCommand);
+    terminal.hide();
+  });
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'terminal-override.activate',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        'Terminal override has been activated!'
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
